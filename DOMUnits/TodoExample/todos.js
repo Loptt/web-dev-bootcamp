dc882ed30ae60e8f08a5bb16b78942ce52f666ab
Carlos Estrada <carlos.deg02@hotmail.com>
2018-11-24 10:38:31 -0600

    Finished todos project

--- DOMUnits/TodoExample/todos.js
+++ DOMUnits/TodoExample/todos.js
@@ -2,11 +2,15 @@ var lis = document.querySelectorAll("li");
 
 for (var i = 0; i < lis.length; ++i) {
     lis[i].addEventListener("mouseover", function(){
-        this.style.color = "green";
+        this.classList.add("selected");
     });
     
     lis[i].addEventListener("mouseout", function(){
-        this.style.color = "black";
+        this.classList.remove("selected");
+    });
+
+    lis[i].addEventListener("click", function(){
+        this.classList.toggle("done");
     });
 }
 
